86290e5c1d67d87211b3e9f413f0fdbc0bb20e7e
Jodavaga <jodavaga@gmail.com>
2018-01-13 10:27:14 -0500

    Admin pro base project, first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router/src/directives/router_link';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
