4a98847d59cc851270022ae0825784ce4bc1f593
Jodavaga <jodavaga@gmail.com>
2018-01-13 11:31:42 -0500

    Cleaned project, comments deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,7 @@ import { APP_ROUTES } from './app.routes';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -26,14 +18,6 @@ import { RegisterComponent } from './login/register.component';
     AppComponent,
     LoginComponent,
     RegisterComponent
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graficas1Component,
-    // PagesComponent,
-    // NopagefoundComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
